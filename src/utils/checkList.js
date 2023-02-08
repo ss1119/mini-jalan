ba1f8fabd6720becd52bbb175eaef7602d3986de
ss1119 <y.sasaki111919@gmail.com>
2023-02-08 21:38:02 +0900

    詳細条件を定義

---
+++ src/utils/checkList.js
@@ -0,0 +1,9 @@
+export const checkList = [
+  { name: "温泉", value: "onsen" },
+  { name: "パーキング無料", value: "parking" },
+  { name: "禁煙部屋", value: "no-smoking" },
+  { name: "喫煙部屋", value: "smoking" },
+  { name: "バス・トイレ付", value: "bathroom" },
+  { name: "朝食付", value: "breakfast" },
+  { name: "夕食付", value: "dinner" },
+];
